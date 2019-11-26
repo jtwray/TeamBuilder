ff07b1c06c4ec7f8bd6d2c164dded5517578abad
James 'Tucker' Wray <tuckerwray84@gmail.com>
2019-11-26 14:53:06 -0500

    Update SingleTeamMate.js

--- teambuilder/src/components/SingleTeamMate.js
+++ teambuilder/src/components/SingleTeamMate.js
@@ -1,1 +1,13 @@
+import React form "react"
 
+export const SingleTeammate = ({tm})=>{
+     return(
+            <div>
+                <h2>{tm.name}</h2>
+                <ul>
+                    <li>{tm.email}</li>
+                    <li>{tm.role}</li>
+                </ul>
+            </div> 
+          )
+        } 

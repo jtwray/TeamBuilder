cf3fa0eef18cbcff470c530a4057c81fb0b64712
James 'Tucker' Wray <tuckerwray84@gmail.com>
2019-11-26 19:27:16 -0500

    Update App.js

--- teambuilder/src/App.js
+++ teambuilder/src/App.js
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
 import { Form } from "./components/Form"
-import { TeamMate } from "./components/TeamMate"
+import { AllTeamMates } from "./components/AllTeamMates"
 
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
     <div className="App" >
       <header className="App-header" >
         <Form teammates={teammates} setTeamMates={setTeamMates} />
-        <TeamMate teammates={teammates} />
+        <AllTeamMates teammates={teammates} />
 
       </header>
     </div>
